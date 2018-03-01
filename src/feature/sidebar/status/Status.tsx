eae5f8bf01768e70a118a384005766021e873a88
Eric Butler <eric@codebutler.com>
2018-03-01 15:15:45 -0500

    implemented status page

--- src/feature/sidebar/status/Status.tsx
+++ src/feature/sidebar/status/Status.tsx
@@ -1,5 +1,5 @@
 import { Component, default as React } from 'react';
-import { CardContent, CardHeader } from 'material-ui';
+import { CardContent, CardHeader, GridListTile, Theme, WithStyles } from 'material-ui';
 import IconButton from 'material-ui/IconButton';
 import Card from 'material-ui/Card';
 import CloseIcon from 'material-ui-icons/Close';
@@ -7,16 +7,35 @@ import { RootAction } from '../../../shared/actions';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import DISTRICTS from '../../../shared/data/districts-info.json';
+import District from '../../../shared/models/District';
+import _ from 'lodash';
+import red from 'material-ui/colors/red';
+import green from 'material-ui/colors/green';
+import withStyles, { ClassNameMap } from 'material-ui/styles/withStyles';
+import GridList from 'material-ui/GridList';
 
 interface DispatchProps {
     onCloseClicked: () => void;
 }
 
-type Props = DispatchProps;
+type ClassKey =
+    | 'cardContentRoot'
+    | 'tileBad'
+    | 'tileGood';
 
-class Status extends Component<Props> {
+type Props = DispatchProps;
+type PropsWithStyles = Props & WithStyles<ClassKey>;
+
+class Status extends Component<PropsWithStyles> {
+
+    private static classForTile(district: District, classes: ClassNameMap<ClassKey>): string {
+        const isGood = !!(district.calendar && district.calendar.googleCalendarId);
+        return isGood ? classes.tileGood : classes.tileBad;
+    }
 
     render() {
+        const { classes } = this.props;
         return (
             <Card>
                 <CardHeader
@@ -25,10 +44,25 @@ class Status extends Component<Props> {
                             <CloseIcon/>
                         </IconButton>
                     )}
-                    title="Status"
+                    title="Calendar Scraper Status"
                 />
-                <CardContent>
-                    hello.
+                <CardContent classes={{root: classes.cardContentRoot}}>
+                    <GridList
+                        cols={2}
+                        cellHeight={40}
+                    >
+                        {
+                            _.toPairs(DISTRICTS).map(([key, district]: [string, District]) => (
+                                <GridListTile
+                                    key={key}
+                                    classes={{tile: Status.classForTile(district, classes)}}
+                                    onClick={() => alert(JSON.stringify(district, null, '  '))}
+                                >
+                                    {`${district.borough} CB${district.number}`}
+                                </GridListTile>
+                            ))
+                        }
+                    </GridList>
                 </CardContent>
             </Card>
         );
@@ -43,4 +77,33 @@ const mapDispatchToProps = (dispatch: Dispatch<RootAction>): DispatchProps => {
     };
 };
 
-export default connect(null, mapDispatchToProps)(Status);
+const styles = (theme: Theme) => ({
+    cardContentRoot: {
+        paddingTop: 0
+    },
+    tileGood: {
+        display: 'flex',
+        justifyContent: 'center' as 'center',
+        alignItems: 'center' as 'center',
+        cursor: 'pointer',
+
+        backgroundColor: green[300],
+        '&:hover': {
+            backgroundColor: green[400],
+        }
+    },
+    tileBad: {
+        // FIXME: how to avoid copy/paste??
+        display: 'flex',
+        justifyContent: 'center' as 'center',
+        alignItems: 'center' as 'center',
+        cursor: 'pointer',
+
+        backgroundColor: red[300],
+        '&:hover': {
+            backgroundColor: red[400],
+        }
+    }
+});
+
+export default connect(null, mapDispatchToProps)(withStyles(styles)<Props>(Status));
